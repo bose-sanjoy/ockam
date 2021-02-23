d053de64890881bd7516baffa0a67e0ce6a28159
Jared Stanbrough <jared@ockam.io>
2021-02-23 13:33:10 -0800

    docs(rust): update worker example

--- implementations/rust/ockam/ockam_examples/examples/guide_01_workers.rs
+++ implementations/rust/ockam/ockam_examples/examples/guide_01_workers.rs
@@ -1,3 +1,24 @@
-fn main() {
-    unimplemented!()
+use ockam::{Context, Result, Worker};
+
+struct Echoer;
+
+impl Worker for Echoer {
+    type Message = String;
+    type Context = Context;
+
+    fn handle_message(&mut self, ctx: &mut Context, msg: String) -> Result<()> {
+        ctx.send_message("app", format!("{}", msg))
+    }
+}
+
+#[ockam::node]
+async fn main(mut ctx: Context) -> Result<()> {
+    ctx.start_worker("echoer", Echoer)?;
+
+    ctx.send_message("echoer", "Hello Ockam!".to_string())?;
+
+    let reply = ctx.receive::<String>()?;
+    println!("Reply: {}", reply);
+
+    ctx.stop()
 }
